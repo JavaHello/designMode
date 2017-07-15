1c5541ed27273931671591e0c12f415111f18cf8
JavaHello <272389184Z@qq.com>
2017-07-15 19:21:41 +0800

    test

--- designMode-web/src/test/java/com/openfree/BaseServiceTest.java
+++ designMode-web/src/test/java/com/openfree/BaseServiceTest.java
@@ -1,15 +1,22 @@
 package com.openfree;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by luokai on 17-7-14.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:spring/applicationContext.xml")
-@TransactionConfiguration(defaultRollback = true)
+@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
+@Transactional
 public class BaseServiceTest {
+    @Test
+    public void test1(){
+
+    }
 }
